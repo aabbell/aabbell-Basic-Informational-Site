d31159b0fd7f1aad5e9d1a83b60e507ad664b6db
aabbell <abelalemneh68@gmail.com>
2025-04-26 11:17:29 +0300

    changed to express instead of plain node

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require("express")
+const path = require("path")
+
+const app = express()
+
+app.get("/" || "/index",(req,res) => res.sendFile(path.join(__dirname, 'index.html' )))
+app.get("/about",(req,res) => res.sendFile(path.join(__dirname ,'about.html')))
+app.get("/404", (req ,res) => res.sendFile(path.join(__dirname ,'404.html')))
+app.get ("/contact-me", (req,res) => res.sendFile(path.join(__dirname, "contact-me.html")))
+
+const PORT = 8080
+
+app.listen(PORT, () => {
+    console.log(`the express app port ${PORT}`)
+})
